257751f82336dc0f8d20fffd25673534894c3f1e
red2901 <kin.p.yu@gmail.com>
2015-10-16 18:27:24 +0100

    removed some redundant directories

--- etc/vs64.bat
+++
@@ -1,6 +0,0 @@
-rem
-rem this is the configuration for 64 bit windows build
-rem
-
-@echo off
-%comspec% /k ""C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"" amd64
--- ylib/README.md
+++
@@ -1,1 +0,0 @@
-ylib - y not!
