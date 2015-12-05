dfc1653e0cec46b8441fd97281d8f7099d721ec0
red2901 <kin.p.yu@gmail.com>
2015-12-05 00:41:49 +0000

    added cython

---
+++ cppstrings/main.cc
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+
+    std::string s("helloworld");
+
+    std::cout << s << std::endl;;
+    return 0;
+}
